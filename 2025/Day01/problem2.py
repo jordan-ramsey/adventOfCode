b4734bc9efcc373c4205e74c3d07f3aba010b7cf
JordanRamsey <jrams1414@gmail.com>
2025-12-01 23:44:54 -0500

    day 2

---
+++ 2025/Day01/problem2.py
@@ -0,0 +1,17 @@
+dial = 50
+zeroCount = 0
+
+with open("input.txt", "r") as file:
+    for line in file:
+        direction, num = line[0], int(line[1:].strip())
+
+        if direction == "L":
+            if num >= dial:
+                zeroCount += (num // 100) if dial == 0 else 1 + (num - dial) // 100
+                dial = (dial - num) % 100
+        elif direction == "R":
+            zeroCount += (dial + num) // 100
+            dial = (dial + num) % 100
+
+
+print(zeroCount)
